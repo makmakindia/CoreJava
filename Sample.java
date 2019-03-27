1506b97f6e0cb4e12e34b103fb9679b5d19afd17
Arun Mannepula <makprotractor@gmail.com>
2019-03-28 02:30:28 +0530

    updated with framework excel and log4j code

--- Sample.java
+++ Sample.java
@@ -3,15 +3,19 @@ package Corejava;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		
-		
-		int a[] = {1,2,3,4,5,10};
-		
-		for(int i : a) {
-			System.out.println("Array : "+a[i]);
+		for(int i=1; i<=5;i++) {
+			System.out.println(i);
 		}
 
+		
+		
+		int i1[] = {10,20,30,40};
+		
+		for(int j : i1) {
+			System.out.println(" i1 array - "+j);
+		}
 	}
 
 }
---
+++ java05OOPS.java
@@ -0,0 +1,29 @@
+package Corejava;
+
+/*
+ Author : Arun 
+ 
+ Abstraction
+Abstraction is the concept of hiding the internal details and describing things in simple terms.
+achieve abstraction in OOPs, such as encapsulation and inheritance.
+
+Encapsulation is used for access restriction to class members and methods.
+
+Access Modifier 	Private 	Default		Protected		public
+
+Inside Class		Y			Y			Y				Y
+Same Pkg Claass		N			Y			Y				Y
+Same Pkg Sub-Class	N			Y			Y				Y
+Other Pkg Class		N			N			Y				Y
+Other Pkg Sub-Class	N			N			N				Y				
+	
+ */
+
+public class java05OOPS {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
